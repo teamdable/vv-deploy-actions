80d26b55bd743680a8a9daf25c8df9d561abb29a
hyeseung-of-dable <hyeseung@dable.io>
2022-03-03 16:36:02 +0900

    resolve #7 & version check하는 중복 코드를 함수로 추출

--- install-check.sh
+++ install-check.sh
@@ -2,20 +2,32 @@
 HOST=$1
 VERSION_FILE=$2
 
-if [[ -z `grep "version" version_check_$HOST.txt` ]]
-then 
-	echo "배포 대상 edge device의 version 파일을 읽어올 수 없습니다."
-	exit 1
-fi
+extract_version() {
+	local check_file=$1
+	
+	raw_version_data=`grep $version_key_string $check_file`
+	if [[ -z $raw_version_data ]]
+	then
+		echo "배포 대상 edge device의 version 파일을 읽어올 수 없습니다."
+		exit 1
+	fi
+	version=`echo $raw_version_data | sed 's/[^a0-9.]//g'`
+	echo $version
+}
 
-source_version=`cat $VERSION_FILE | awk '{print $3}'`
 if [[ $VERSION_FILE == "package.json" ]]
 then
-	target_version=`grep "version" version_check_$HOST.txt | awk '{print $2}' | tr -d ","`
-elif  [[ $VERSION_FILE == ".version" || "_version.py" ]]
+	version_key_string="version"
+elif [[ $VERSION_FILE = ".version" ]] || [[ $VERSION_FILE = "_version.py" ]]
 then
-	target_version=`grep "__version__" version_check_$HOST.txt | awk '{print $3}'`
+	version_key_string="__version__"
+else
+	echo "$VERSION_FILE 버전 메타데이터는 지원하지 않습니다"
+	exit 1
 fi
+source_version=`extract_version $VERSION_FILE`
+target_version=`extract_version version_check_$HOST.txt`
+echo "source version: $source_version, target version: $target_version"
 
 if [[ $source_version == $target_version ]]
 then
