e0fc530f4dcb3f1f29759d5becc5787e8ef757c5
hyeseung-of-dable <hyeseung@dable.io>
2022-03-03 12:43:53 +0900

    package.json에서도 version읽어서 확인할 수 있도록 수정

--- install-check.sh
+++ install-check.sh
@@ -2,14 +2,21 @@
 HOST=$1
 VERSION_FILE=$2
 
-if [[ -z `grep "__version__" version_check_$HOST.txt` ]]
+if [[ -z `grep "version" version_check_$HOST.txt` ]]
 then 
 	echo "배포 대상 edge device의 version 파일을 읽어올 수 없습니다."
 	exit 1
 fi
 
 source_version=`cat $VERSION_FILE | awk '{print $3}'`
-target_version=`grep __version__ version_check_$HOST.txt | awk '{print $3}'`
+if [[ $VERSION_FILE == "package.json" ]]
+then
+	target_version=`grep "version" version_check_$HOST.txt | awk '{print $2}' | tr -d ","`
+elif  [[ $VERSION_FILE == ".version" || "_version.py" ]]
+then
+	target_version=`grep "__version__" version_check_$HOST.txt | awk '{print $3}'`
+fi
+
 if [[ $source_version == $target_version ]]
 then
 	echo "install Success"
