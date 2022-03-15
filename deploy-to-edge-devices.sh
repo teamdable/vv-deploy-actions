2b61737b488cc76323b1978437320a334b64c978
hyeseung-of-dable <hyeseung@dable.io>
2022-03-15 19:10:33 +0900

    refactoring: 인자를 longoption으로 받도록 수정. resolve #4

--- deploy-to-edge-devices.sh
+++ deploy-to-edge-devices.sh
@@ -1,12 +1,53 @@
 #!/bin/bash
-USER=$1
-PASSWORD=$2
-OTP=$3
-ZIP_FILE_NAME=$4
-PARENT_DIR=$5
-CODE_NAME=$6
-VERSION_FILE=$7
-SLACK_CHANNEL=$8
+LONG=user:,password:,otp:,zip-file-name:,parent-dir:,code-name:,version-file:,slack-channel:
+OPTS=$(getopt -o '' -a --longoptions $LONG  -- "$@")
+[ $? -eq 0 ] || {
+    echo "인자전달이 잘못되었습니다. "
+    exit 1
+}
+eval set -- "$OPTS"
+
+while [[ $# -gt 0 ]]
+do
+	case "$1" in
+	--user)
+		USER=$2
+		shift 2
+		;;
+	--password)
+		PASSWORD=$2
+		shift 2
+		;;
+	--otp)
+		OTP=$2
+		shift 2
+		;;
+	--zip-file-name)
+		ZIP_FILE_NAME=$2
+		shift 2
+		;;
+	--parent-dir)
+		PARENT_DIR=$2
+		shift 2
+		;;
+	--code-name)
+		CODE_NAME=$2
+		shift 2
+		;;
+	--version-file)
+		VERSION_FILE=$2
+		shift 2
+		;;
+	--slack-channel)
+		SLACK_CHANNEL=$2
+		shift 2
+		;;
+	--)
+		shift
+		break
+		;;
+	esac
+done
 
 NOT_DEPLOYED_HOST=()
 NOT_INSTALLED_HOST=()
@@ -20,7 +61,7 @@ do
 	# 1-1. deploy check
 	action/deploy-check.exp $USER $HOST $PASSWORD $OTP $ZIP_FILE_NAME > deploy_check_$HOST.txt
 	sleep 10
-	action/deploy-check.sh $HOST $ZIP_FILE_NAME
+	action/deploy-check.sh --host $HOST --zip-file-name $ZIP_FILE_NAME
 	result=$?
 
 	if [[ $result -eq 1 ]]
@@ -34,7 +75,7 @@ do
 		# 2-2. install check 
 		action/install-check.exp $USER $HOST $PASSWORD $OTP $PARENT_DIR $CODE_NAME $VERSION_FILE > version_check_$HOST.txt
 		sleep 10
-		action/install-check.sh $HOST $VERSION_FILE
+		action/install-check.sh --host $HOST --version-file $VERSION_FILE
 		result=$?
 
 		if [[ $result -eq 1 ]]
