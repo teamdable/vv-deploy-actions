49d1698e8ceff80322a47c15db621ad7ad6b4d05
hyeseung-of-dable <hyeseung@dable.io>
2022-04-11 16:34:48 +0900

    metadata version 업데이트하는 스크립트 추가

---
+++ update-version.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+LONG=code-name:,update-version:
+OPTS=$(getopt -o '' -a --longoptions $LONG  -- "$@")
+[ $? -eq 0 ] || {
+    echo "인자전달이 잘못되었습니다. "
+    exit 1
+}
+eval set -- "$OPTS"
+
+while [[ $# -gt 0 ]]
+do
+	case "$1" in
+	--code-name)
+		CODE_NAME=$2
+		shift 2
+		;;
+	--update-version)
+		VERSION_FILE=$2
+		shift 2
+		;;
+	--)
+		shift
+		break
+		;;
+	esac
+done
+
+
+if [[ $CODE_NAME == "edge-player" ]]
+then
+	MODULE_NAME="edge-player"
+elif [[ $CODE_NAME == "resource" ]]
+then
+	MODULE_NAME="resource-monitoring"
+elif [[ $CODE_NAME == "process" ]]
+then
+	MODULE_NAME="process-monitoring"
+elif [[ $CODE_NAME == "vv-yolor" ]]
+then
+	MODULE_NAME="inference"
+elif [[ $CODE_NAME == "vv-edge-setup" ]]
+then
+	MODULE_NAME=$CODE_NAME
+fi
+
+sed -i "s/$MODULE_NAME: [0-9]*\.[0-9]*\.[0-9]*[(a|b|rc)]*[0-9]*/$MODULE_NAME: $UPDATE_VERSION/g" ~/.metadata
