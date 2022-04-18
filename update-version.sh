4c236d93a92889ef42ea83f51b22a8575ff5511b
hyeseung-of-dable <hyeseung@dable.io>
2022-04-18 16:53:06 +0900

    code-name으로 metadata저장하도록 수정

--- update-version.sh
+++ update-version.sh
@@ -21,22 +21,5 @@ do
 	esac
 done
 
-
-if [[ $CODE_NAME == "edge-player" ]]
-then
-	MODULE_NAME="edge-player"
-elif [[ $CODE_NAME == "resource" ]]
-then
-	MODULE_NAME="resource-monitoring"
-elif [[ $CODE_NAME == "process" ]]
-then
-	MODULE_NAME="process-monitoring"
-elif [[ $CODE_NAME == "vv-yolor" ]]
-then
-	MODULE_NAME="inference"
-elif [[ $CODE_NAME == "vv-edge-setup" ]]
-then
-	MODULE_NAME=$CODE_NAME
-fi
 UPDATE_VERSION=`cat only-version`
-sed -i "s/$MODULE_NAME: [0-9]\+\.[0-9]\+\.[0-9]\+[(a|b|rc)]*[0-9]*/$MODULE_NAME: $UPDATE_VERSION/g" ~/.metadata
+sed -i "s/$CODE_NAME: [0-9]\+\.[0-9]\+\.[0-9]\+[(a|b|rc)]*[0-9]*/$CODE_NAME: $UPDATE_VERSION/g" ~/.metadata
