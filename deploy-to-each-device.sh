de564aff149469a20582c194bc88c233f0dbe757
yunseung-dable <yunseung@dable.io>
2022-04-19 18:27:03 +0900

    outputs 코드 수정 다음 스텝을 위한 inputs 항목 추가 output 으로 list를 받기 위해 verbose 옵션 추가

--- deploy-to-each-device.sh
+++ deploy-to-each-device.sh
@@ -1,8 +1,29 @@
 #!/bin/bash
 
 # 인자받는 부분
-getopt -o -a -l --
+LONG=vpn_ip:
+OPTS=$(getopt -o '' -a --longoptions $LONG  -- "$@")
+[ $? -eq 0 ] || {
+    echo "인자전달이 잘못되었습니다. "
+    exit 1
+}
+eval set -- "$OPTS"
 
+while [[ $# -gt 0 ]]
+do
+	case "$1" in
+	--vpn_ip)
+		VPN_IP=$2
+		shift 2
+		;;
+	--)
+		shift
+		break
+		;;
+	esac
+done
+
+echo "$VPN_IP"
 
 # /tmp 디렉토리 scp 전달 + unzip
 
