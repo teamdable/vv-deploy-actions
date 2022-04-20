2cdb9b0f7f07ca52024829aa6b6d32dcfa458d50
yunseung-dable <yunseung@dable.io>
2022-04-20 17:00:30 +0900

    ssh 접속후 scp, install 작업 적용 edge-setup check-file-existence 한글로 인해 일단 보류

--- deploy-to-each-device.sh
+++ deploy-to-each-device.sh
@@ -13,7 +13,7 @@ while [[ $# -gt 0 ]]
 do
 	case "$1" in
 	--vpn-ip)
-		VPN_IP=$2
+		HOST=$2
 		shift 2
 		;;
   --user)
@@ -55,13 +55,45 @@ do
 	esac
 done
 
-echo "$VPN_IP"
+echo "$HOST"
 
 # /tmp 디렉토리 scp 전달 + unzip
 
-echo "hostname: $VPN_IP"
+echo "hostname: $HOST"
 
-action/deploy.exp "$USER" "$VPN_IP" "$PASSWORD" "$OTP" "$ZIP_FILE_NAME" > /dev/null
+action/deploy.exp "$USER" "$HOST" "$PASSWORD" "$OTP" "$ZIP_FILE_NAME" > /dev/null
+
+## 1-1. deploy check
+#action/deploy-check.exp "$USER" "$HOST" "$PASSWORD" "$OTP" "$ZIP_FILE_NAME" > deploy_check_"$HOST".txt
+#sleep 10
+#action/deploy-check.sh --host "$HOST" --zip-file-name "$ZIP_FILE_NAME"
+deploy_result=$?
+
+if [[ $deploy_result -eq 1 ]]
+then
+  echo "1단계 deploy 작업에 실패했습니다"
+  exit 1
+fi
+
+# 2. install - edge서버에서 해당 모듈을 사용할 수 있도록 압축 해제 & 패키지 설치
+action/install.exp $USER $HOST $PASSWORD $OTP $ZIP_FILE_NAME $PARENT_DIR $CODE_NAME > install_check_$HOST.txt
+sleep 10
+# 2-2. install check
+action/install-check.exp $USER $HOST $PASSWORD $OTP $PARENT_DIR $CODE_NAME $VERSION_FILE > version_check_$HOST.txt
+action/install-check.sh --host $HOST --version-file $VERSION_FILE
+install_result=$?
+
+if [[ $install_result -eq 1 ]]
+then
+  echo "2단계 install 작업에 실패했습니다"
+  exit 1
+fi
+
+echo -e "\n"
+echo "deploy, install 작업에 모두 성공했습니다"
+exit 0
+
+
 # kill
 
 # deploy
