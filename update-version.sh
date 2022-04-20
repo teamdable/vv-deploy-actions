c86acadbec8db6e029bb6b855cc58528bb43bb68
hyeseung-of-dable <hyeseung@dable.io>
2022-04-20 18:17:46 +0900

    inferenceg프로세스명 매핑

--- update-version.sh
+++ update-version.sh
@@ -28,6 +28,9 @@ then
 elif [[ $CODE_NAME == "resource" ]]
 then
 	CODE_NAME="resource-monitoring"
+elif [[ $CODE_NAME == "vv-yolor" ]]
+then
+	CODE_NAME="inference"
 fi
 
 sed -i "s/$CODE_NAME: [0-9]\+\.[0-9]\+\.[0-9]\+[(a|b|rc)]*[0-9]*/$CODE_NAME: $UPDATE_VERSION/g" ~/.metadata
